15030bd48909f4a3ba2af93989a50a8c913c51a7
margb13 <marg.b13@hotmail.com>
2018-02-17 17:30:18 -0500

    Eliminada carpeta node_modules del repo

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const server = app.listen(3000, listening);
-
-function listening() {
-  console.log('Listening :)');
-};
-
-app.use(express.static('public'));
